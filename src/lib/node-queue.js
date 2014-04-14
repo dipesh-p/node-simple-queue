b918921a271ed35f1b35b7853969268f432ebd89
Chiarg Shah <chirag.sweng@gmail.com>
2014-04-14 18:34:12 +0530

    added UI for queues, it shows number of processes and worked working

--- src/lib/node-queue.js
+++ src/lib/node-queue.js
@@ -46,5 +46,74 @@ function restartWorker(queue,pid,db_config){
 
 	});
 }
+
+exports.NodeQueueWeb=function(req,res, db_config){
+	db = new DB('MongoDB');
+	var action = req.query.page;
+	switch(action){
+		case 'first':
+			jobs_overview(req, res);
+			break;
+		case 'jobs':
+			jobs_list(req, res);
+			break;
+		case 'workers':
+			workers_list(req, res);
+			break;
+		default:
+			render_html(req, res);
+			break;
+	}
+
+};
+
+function render_html(req, res){
+	var fs = require('fs');
+	fs.readFile(__dirname + '/views/node_queue_web/node_queue.txt', 'utf8', function (err,data) {
+		res.send(data);
+	});
+}
+
+function jobs_overview(req, res){
+	console.log("TEST DATA");
+	db = new DB('MongoDB');
+	var group = {
+	   key: {QUEUE:1, STATUS: 1},
+	   cond: {},
+	   reduce: function(curr,result) { result.count++;},
+	   initial: {count: 0}
+	};
+	db.Job.collection.group(group.key, group.cond, group.initial, group.reduce, true, true, function(err, results) {
+		if(err){
+			res.send({status: 'failure'});
+		} else {
+			res.send({status: 'success', data: results});
+		}
+	});
+}
+
+
+function jobs_list(req, res){
+	var queue_id = req.query.queue_id;
+	var status = req.query.status;
+
+	db.Job.find({QUEUE: queue_id, STATUS: status}, function(err, jobs){
+		console.log(jobs);
+		if(err)
+			res.send({status: 'failure'});
+		else
+			res.send({status: 'success', data: jobs});
+	})
+}
+
+function workers_list(req, res){
+	db.Worker.find({}, function(err, workers){
+		if(err)
+			res.send({status: 'failure'});
+		else
+			res.send({status: 'success', data: workers});
+	})
+}
+
 exports.NodeWorker=NodeWorker;
 exports.NodeQueue=NodeQueue;
